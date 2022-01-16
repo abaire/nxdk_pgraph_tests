ec771831c990303208a6833c3270bf57ac763c49
Erik Abair <erik.abair@gmail.com>
2022-01-15 21:23:05 -0800

    Fix fog param type. Cleanup.

--- src/shaders/shader_program.cpp
+++ src/shaders/shader_program.cpp
@@ -7,17 +7,14 @@
 #include "pbkit_ext.h"
 
 void ShaderProgram::LoadShaderProgram(const uint32_t *shader, uint32_t shader_size) const {
-  /* Load the shader we will render with */
   uint32_t *p;
   int i;
 
-  /* Setup vertex shader */
   p = pb_begin();
 
-  /* Set run address of shader */
+  // Set run address of shader
   p = pb_push1(p, NV097_SET_TRANSFORM_PROGRAM_START, 0);
 
-  /* Set execution mode */
   p = pb_push1(
       p, NV097_SET_TRANSFORM_EXECUTION_MODE,
       MASK(NV097_SET_TRANSFORM_EXECUTION_MODE_MODE, NV097_SET_TRANSFORM_EXECUTION_MODE_MODE_PROGRAM) |
@@ -26,12 +23,11 @@ void ShaderProgram::LoadShaderProgram(const uint32_t *shader, uint32_t shader_si
   p = pb_push1(p, NV097_SET_TRANSFORM_PROGRAM_CXT_WRITE_EN, 0);
   pb_end(p);
 
-  /* Set cursor and begin copying program */
+  // Set cursor and begin copying program
   p = pb_begin();
   p = pb_push1(p, NV097_SET_TRANSFORM_PROGRAM_LOAD, 0);
   pb_end(p);
 
-  /* Copy program instructions (16-bytes each) */
   for (i = 0; i < shader_size / 16; i++) {
     p = pb_begin();
     pb_push(p++, NV097_SET_TRANSFORM_PROGRAM, 4);
