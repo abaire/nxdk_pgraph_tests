7998c1b904f9cdf62f7747190fdab4e2a2eecf31
Erik Abair <erik.abair@gmail.com>
2022-05-11 06:37:38 -0700

    Fixes warnings and reduces function scope.

--- src/tests/attribute_float_tests.cpp
+++ src/tests/attribute_float_tests.cpp
@@ -1,20 +1,18 @@
 #include "attribute_float_tests.h"
 
 #include <pbkit/pbkit.h>
+
 #include <vector>
 
-#include "../test_host.h"
-#include "debug_output.h"
 #include "pbkit_ext.h"
 #include "shaders/precalculated_vertex_shader.h"
-#include "shaders/pixel_shader_program.h"
 
 // clang format off
-static const std::vector<uint32_t> passthrough {
+static const std::vector<uint32_t> passthrough{
 #include "shaders/precalculated_vertex_shader_4c_texcoords.inl"
 };
 
-static const std::vector<uint32_t> mulColour {
+static const std::vector<uint32_t> mulColour{
 #include "shaders/mul_col0_by_const0_vertex_shader.inl"
 };
 // clang format on
@@ -32,30 +30,28 @@ static constexpr uint32_t negNanS = 0xFF800001;
 static constexpr uint32_t posMax = 0x7F7FFFFF;
 static constexpr uint32_t negMax = 0xFF7FFFFF;
 // Min normal
-static constexpr uint32_t posMinNorm= 0x00800000;
+static constexpr uint32_t posMinNorm = 0x00800000;
 static constexpr uint32_t negMinNorm = 0x80800000;
 // Max subnormal
 static constexpr uint32_t posMaxSubNorm = 0x007FFFFF;
 static constexpr uint32_t negMaxSubNorm = 0x807FFFFF;
 // Min subnormal
-static constexpr uint32_t posMinSubNorm= 0x00000001;
+static constexpr uint32_t posMinSubNorm = 0x00000001;
 static constexpr uint32_t negMinSubNorm = 0x80000001;
 
-float f(uint32_t v) {
-    return *((float *)&v);
-}
+static float f(uint32_t v) { return *((float *)&v); }
 
 struct TestConfig {
-  const char *fileName; // must be valid for a filename
+  const char *fileName;  // must be valid for a filename
   const char *description;
   float attribute_value[2];
 };
 
-static const TestConfig testConfigs[] {
-    { "0_1",  "0 to 1", {  0.f, 1.f }},
-    {"-1_1", "-1 to 1", { -1.f, 1.f }},
-    {"-8_1", "-8 to 1", { -8.f, 1.f }},
-    { "0_8",  "0 to 8", {  0.f, 8.f }},
+static const TestConfig testConfigs[]{
+    {"0_1", "0 to 1", {0.f, 1.f}},
+    {"-1_1", "-1 to 1", {-1.f, 1.f}},
+    {"-8_1", "-8 to 1", {-8.f, 1.f}},
+    {"0_8", "0 to 8", {0.f, 8.f}},
     {"-NaNq_NaNq", "-NaN to +NaN (quiet)", {f(negNanQ), f(posNanQ)}},
     {"-NaNs_NaNs", "-NaN to +NaN (signalling)", {f(negNanS), f(posNanS)}},
     {"-INF_INF", "-INF to +INF", {f(negInf), f(posInf)}},
@@ -72,8 +68,7 @@ AttributeFloatTests::AttributeFloatTests(TestHost &host, std::string output_dir)
   }
 }
 
-void CreateGeometry(float x, float y, float width, float height, float from, float to, std::vector<float> &vb) {
-
+static void CreateGeometry(float x, float y, float width, float height, float from, float to, std::vector<float> &vb) {
   float right = x + width;
   float bottom = y + height;
 
@@ -99,42 +94,28 @@ void AttributeFloatTests::Test(const TestConfig &tConfig) {
 
   // Multipliers for mul shader...
   const std::vector<std::pair<const char *, float>> mulVals = {
-    {"1.0", 1.f},
-    {"0.0", 0.f},
-    {"-INF", f(negInf)},
-    {"INF", f(posInf)},
-    {"-NaNq", f(negNanQ)},
-    {"NaNq", f(posNanQ)}
-  };
+      {"1.0", 1.f}, {"0.0", 0.f}, {"-INF", f(negInf)}, {"INF", f(posInf)}, {"-NaNq", f(negNanQ)}, {"NaNq", f(posNanQ)}};
   std::vector<std::shared_ptr<VertexShaderProgram>> vss;
 
   float inset = 0.2f;
   float totalWidth = (1.f - 2 * inset);
-  float colWidth = totalWidth / mulVals.size();
+  float colWidth = totalWidth / static_cast<float>(mulVals.size());
 
   for (int i = 0; i < 1 + mulVals.size(); i++) {
     vb.clear();
 
-    float left = inset + i * colWidth;
+    float left = inset + static_cast<float>(i) * colWidth;
 
     // Create geometry
-    CreateGeometry(
-      left * fb_width,
-      (inset + 0.1f) * fb_height,
-      (colWidth * 0.8) * fb_width,
-      (1 - 2 * inset) * fb_height,
-      tConfig.attribute_value[0],
-      tConfig.attribute_value[1],
-      vb
-    );
+    CreateGeometry(left * fb_width, (inset + 0.1f) * fb_height, (colWidth * 0.8f) * fb_width,
+                   (1 - 2 * inset) * fb_height, tConfig.attribute_value[0], tConfig.attribute_value[1], vb);
 
     // Set VS
     // First use a passthrough shader
     // Then use a shader that multiplies the colour
     auto vs = std::make_shared<PrecalculatedVertexShader>();
     bool mulShader = i != 0;
-    float mul;
-    const std::vector<uint32_t>* shader;
+    const std::vector<uint32_t> *shader;
     if (!mulShader) {
       shader = &passthrough;
     } else {
@@ -154,17 +135,12 @@ void AttributeFloatTests::Test(const TestConfig &tConfig) {
       host_.SetVertex(&vb[v]);
     }
     host_.End();
-
   }
 
   // Draw descriptive text
   std::string fileName = std::string(tConfig.fileName);
-  pb_print("%s\n%s\n0x%08x to 0x%08x\n",
-    fileName.c_str(),
-    tConfig.description,
-    *(uint32_t*)&tConfig.attribute_value[0],
-    *(uint32_t*)&tConfig.attribute_value[1]
-  );
+  pb_print("%s\n%s\n0x%08x to 0x%08x\n", fileName.c_str(), tConfig.description,
+           *(uint32_t *)&tConfig.attribute_value[0], *(uint32_t *)&tConfig.attribute_value[1]);
   pb_print("Multiplier: n/a");
   for (auto &m : mulVals) {
     pb_print(" | %s", m.first);
