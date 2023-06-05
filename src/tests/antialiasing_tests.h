38253e28c2d31e3475d1b53c54ab77e4b3bb776a
Erik Abair <erik.abair@gmail.com>
2023-06-04 21:55:55 -0700

    Adds Makefile option to enable MultiframeCPUBlitTest.

--- src/tests/antialiasing_tests.h
+++ src/tests/antialiasing_tests.h
@@ -35,7 +35,6 @@ class AntialiasingTests : public TestSuite {
   void TestGPUAAWriteAfterCPUWrite();
   void TestNonAACPURoundTrip();
 
-//#define MULTIFRAME_CPU_BLIT
 #ifdef MULTIFRAME_CPU_BLIT
   // This test is only useful when viewing live and should generally be disabled.
   void TestMultiframeCPUBlit();
