23960fa6412325e11993789a260436dfb741b6ea
Erik Abair <erik.abair@gmail.com>
2023-07-16 22:03:24 -0700

    Adds test for 0x16BC glEdgeFlag.
    Fixes #104

---
+++ src/tests/edge_flag_tests.h
@@ -0,0 +1,25 @@
+#ifndef NXDK_PGRAPH_TESTS_EDGE_FLAG_TESTS_H
+#define NXDK_PGRAPH_TESTS_EDGE_FLAG_TESTS_H
+
+#include <memory>
+#include <vector>
+
+#include "test_host.h"
+#include "test_suite.h"
+
+class TestHost;
+class VertexBuffer;
+
+// Tests behavior of 0x16BC - glEdgeFlag
+class EdgeFlagTests : public TestSuite {
+ public:
+  EdgeFlagTests(TestHost& host, std::string output_dir);
+
+  void Initialize() override;
+  void Deinitialize() override;
+
+ private:
+  void Test(const std::string& name, bool edge_flag);
+};
+
+#endif  // NXDK_PGRAPH_TESTS_EDGE_FLAG_TESTS_H
