1e95c75da433849612d4baf376b7315d778a0481
Erik Abair <erik.abair@gmail.com>
2025-09-17 09:27:19 -0700

    Adds tests for NV097_CLEAR_SURFACE_COLOR interaction with surface format.

--- src/tests/clear_tests.h
+++ src/tests/clear_tests.h
@@ -1,20 +1,24 @@
 #ifndef NXDK_PGRAPH_TESTS_CLEAR_TESTS_H
 #define NXDK_PGRAPH_TESTS_CLEAR_TESTS_H
 
+#include "test_host.h"
 #include "test_suite.h"
-
-class TestHost;
 
 /**
  * Tests the behavior of various write masks on NV097_CLEAR_SURFACE.
  *
- * Each test draws some geometry, then modifies the NV097_SET_COLOR_MASK,
- * NV097_SET_DEPTH_MASK, and NV097_SET_STENCIL_MASK settings, then invokes
- * NV097_CLEAR_SURFACE.
+ * Color mask (C*) tests:
+ *   Each test draws some geometry, then modifies the NV097_SET_COLOR_MASK,
+ *   NV097_SET_DEPTH_MASK, and NV097_SET_STENCIL_MASK settings, then invokes
+ *   NV097_CLEAR_SURFACE.
  *
- * In all cases, the clear color is set to 0x7F7F7F7F and the depth value is
- * set to 0xFFFFFFFF.
+ *   In all cases, the clear color is set to 0x7F7F7F7F and the depth value is
+ *   set to 0xFFFFFFFF.
  *
+ * Surface format color (SFC*) tests:
+ *   Each test clears a render target surface and then renders it as a texture
+ *   with alpha blending disabled in order to test the interaction of surface
+ *   format with NV097_CLEAR_SURFACE_COLOR.
  */
 class ClearTests : public TestSuite {
  public:
@@ -24,8 +28,8 @@ class ClearTests : public TestSuite {
 
  private:
   void CreateGeometry();
-  void Test(uint32_t color_mask, bool depth_write_enable);
 
-  static std::string MakeTestName(uint32_t color_mask, bool depth_write_enable);
+  void TestColorMask(uint32_t color_mask, bool depth_write_enable);
+  void TestSurfaceFmt(TestHost::SurfaceColorFormat surface_format, const std::string& name);
 };
 #endif  // NXDK_PGRAPH_TESTS_CLEAR_TESTS_H
