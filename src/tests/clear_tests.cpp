76eec60280e315bb36487f4a9bf4321c47cb9ac9
Erik Abair <abaire@users.noreply.github.com>
2025-09-17 09:31:56 -0700

    Adds tests for NV097_CLEAR_SURFACE_COLOR interaction with surface format. (#249)

--- src/tests/clear_tests.cpp
+++ src/tests/clear_tests.cpp
@@ -21,6 +21,27 @@ static constexpr uint32_t kColorMasks[] = {
         NV097_SET_COLOR_MASK_RED_WRITE_ENABLE | NV097_SET_COLOR_MASK_ALPHA_WRITE_ENABLE,
 };
 
+static std::string MakeMaskTestName(uint32_t color_mask, bool depth_write_enable) {
+  char buf[32] = {0};
+  snprintf(buf, 31, "C%08X_Depth_%s", color_mask, depth_write_enable ? "y" : "n");
+  return buf;
+}
+
+struct SurfaceFormatTest {
+  const char* test_name;
+  TestHost::SurfaceColorFormat surface_format;
+};
+static constexpr SurfaceFormatTest kSurfaceFormatTests[] = {
+    {"SFC_A8R8G8B8", TestHost::SurfaceColorFormat::SCF_A8R8G8B8},
+    {"SFC_X1R5G5B5_Z1R5G5B5", TestHost::SurfaceColorFormat::SCF_X1R5G5B5_Z1R5G5B5},
+    {"SCF_X1R5G5B5_O1R5G5B5", TestHost::SurfaceColorFormat::SCF_X1R5G5B5_O1R5G5B5},
+    {"SCF_R5G6B5", TestHost::SurfaceColorFormat::SCF_R5G6B5},
+    {"SCF_X8R8G8B8_Z8R8G8B8", TestHost::SurfaceColorFormat::SCF_X8R8G8B8_Z8R8G8B8},
+    {"SCF_X8R8G8B8_O8R8G8B8", TestHost::SurfaceColorFormat::SCF_X8R8G8B8_O8R8G8B8},
+    {"SCF_X1A7R8G8B8_Z1A7R8G8B8", TestHost::SurfaceColorFormat::SCF_X1A7R8G8B8_Z1A7R8G8B8},
+    {"SCF_X1A7R8G8B8_O1A7R8G8B8", TestHost::SurfaceColorFormat::SCF_X1A7R8G8B8_O1A7R8G8B8},
+};
+
 /**
  * Constructs the test suite and creates test cases.
  *
@@ -120,14 +141,52 @@ static constexpr uint32_t kColorMasks[] = {
  *  Z-buffer result after enabling writing to all color channels and the
  *  depth/stencil buffer.
  *
+ * @tc SFC_A8R8G8B8
+ *   Tests the effect of clear color on LE_A8R8G8B8 surface format.
+ *
+ * @tc SFC_X1R5G5B5_Z1R5G5B5
+ *   Tests the effect of clear color on LE_X1R5G5B5_Z1R5G5B5 surface format.
+ *   The cleared surface will have its most significant bit set to 0.
+ *   (Note that the surface is rendered as an ARGB8 texture)
+ *
+ * @tc SFC_X1R5G5B5_O1R5G5B5
+ *   Tests the effect of clear color on LE_X1R5G5B5_O1R5G5B5 surface format.
+ *   The cleared surface will have its most significant bit set to 1.
+ *   (Note that the surface is rendered as an ARGB8 texture)
+ *
+ * @tc SCF_R5G6B5
+ *   Tests the effect of clear color on LE_R5G6B5 surface format.
+ *   (Note that the surface is rendered as an ARGB8 texture)
+ *
+ * @tc SCF_X8R8G8B8_Z8R8G8B8
+ *   Tests the effect of clear color on LE_X8R8G8B8_Z8R8G8B8 surface format.
+ *   The cleared surface will have its most significant byte set to 0.
+ *
+ * @tc SCF_X8R8G8B8_O8R8G8B8
+ *   Tests the effect of clear color on LE_X8R8G8B8_O8R8G8B8 surface format.
+ *   The cleared surface will have its most significant byte set to 1.
+ *
+ * @tc SCF_X1A7R8G8B8_Z1A7R8G8B8
+ *   Tests the effect of clear color on LE_X1A7R8G8B8_Z1A7R8G8B8 surface format.
+ *   The cleared surface will have its most significant bit set to 0.
+ *
+ * @tc SCF_X1A7R8G8B8_O1A7R8G8B8
+ *   Tests the effect of clear color on LE_X1A7R8G8B8_O1A7R8G8B8 surface format.
+ *   The cleared surface will have its most significant bit set to 1.
  */
 ClearTests::ClearTests(TestHost& host, std::string output_dir, const Config& config)
     : TestSuite(host, std::move(output_dir), "Clear", config) {
   for (auto color_write : kColorMasks) {
     for (auto depth_write : {true, false}) {
-      std::string name = MakeTestName(color_write, depth_write);
-      tests_[name] = [this, color_write, depth_write]() { Test(color_write, depth_write); };
+      std::string name = MakeMaskTestName(color_write, depth_write);
+      tests_[name] = [this, color_write, depth_write]() { TestColorMask(color_write, depth_write); };
     }
+  }
+
+  for (auto& test_info : kSurfaceFormatTests) {
+    tests_[test_info.test_name] = [this, &test_info]() {
+      TestSurfaceFmt(test_info.surface_format, test_info.test_name);
+    };
   }
 }
 
@@ -170,7 +229,7 @@ void ClearTests::CreateGeometry() {
   }
 }
 
-void ClearTests::Test(uint32_t color_mask, bool depth_write_enable) {
+void ClearTests::TestColorMask(uint32_t color_mask, bool depth_write_enable) {
   host_.PrepareDraw();
   host_.DrawArrays(TestHost::POSITION | TestHost::DIFFUSE);
 
@@ -199,12 +258,107 @@ void ClearTests::Test(uint32_t color_mask, bool depth_write_enable) {
   pb_printat(14, 0, (char*)"the geometry that was rendered.");
   pb_draw_text_screen();
 
-  std::string name = MakeTestName(color_mask, depth_write_enable);
+  std::string name = MakeMaskTestName(color_mask, depth_write_enable);
   host_.FinishDraw(allow_saving_, output_dir_, suite_name_, name, true);
 }
 
-std::string ClearTests::MakeTestName(uint32_t color_mask, bool depth_write_enable) {
-  char buf[32] = {0};
-  snprintf(buf, 31, "C%08X_Depth_%s", color_mask, depth_write_enable ? "y" : "n");
-  return buf;
+void ClearTests::TestSurfaceFmt(TestHost::SurfaceColorFormat surface_format, const std::string& test_name) {
+  static constexpr uint32_t kTextureSize = 128;
+  static constexpr auto kBlackCenterMarkSize = 2.f;
+
+  uint32_t quad_height = kTextureSize;
+  auto pitch = TestHost::GetSurfaceColorPitch(surface_format, 1);
+  if (pitch == 1) {
+    quad_height /= 4;
+  } else if (pitch == 2) {
+    quad_height /= 2;
+  }
+
+  auto clear_texture = [this, surface_format](uint32_t clear_color) {
+    auto target_surface = host_.GetTextureMemoryForStage(0);
+    host_.RenderToSurfaceStart(target_surface, surface_format, kTextureSize, kTextureSize);
+
+    Pushbuffer::Begin();
+    Pushbuffer::Push(NV097_SET_COLOR_CLEAR_VALUE, clear_color);
+    Pushbuffer::Push(NV097_CLEAR_SURFACE, NV097_CLEAR_SURFACE_COLOR);
+    Pushbuffer::End(true);
+
+    host_.SetFinalCombiner0Just(TestHost::SRC_DIFFUSE);
+    host_.SetFinalCombiner1Just(TestHost::SRC_ZERO, true, true);
+
+    host_.Begin(TestHost::PRIMITIVE_QUADS);
+    host_.SetDiffuse(0x00000000);
+    const auto half_texture_size = kTextureSize * 0.5f;
+    host_.SetVertex(half_texture_size - kBlackCenterMarkSize, half_texture_size - kBlackCenterMarkSize, 1.f);
+    host_.SetVertex(half_texture_size + kBlackCenterMarkSize, half_texture_size - kBlackCenterMarkSize, 1.f);
+    host_.SetVertex(half_texture_size + kBlackCenterMarkSize, half_texture_size + kBlackCenterMarkSize, 1.f);
+    host_.SetVertex(half_texture_size - kBlackCenterMarkSize, half_texture_size + kBlackCenterMarkSize, 1.f);
+    host_.End();
+
+    host_.RenderToSurfaceEnd();
+  };
+
+  auto draw_quad = [this, quad_height](float left, float top) {
+    host_.SetBlend(false);
+    host_.SetFinalCombiner0Just(TestHost::SRC_TEX0);
+    host_.SetFinalCombiner1Just(TestHost::SRC_TEX0, true);
+    host_.SetTextureStageEnabled(0, true);
+    host_.SetShaderStageProgram(TestHost::STAGE_2D_PROJECTIVE);
+
+    auto& texture_stage = host_.GetTextureStage(0);
+    texture_stage.SetFormat(GetTextureFormatInfo(NV097_SET_TEXTURE_FORMAT_COLOR_LU_IMAGE_A8B8G8R8));
+    texture_stage.SetImageDimensions(kTextureSize, kTextureSize);
+    host_.SetupTextureStages();
+
+    const float right = left + kTextureSize;
+    const float bottom = top + static_cast<float>(quad_height);
+    static constexpr float kZ = 0.f;
+
+    host_.Begin(TestHost::PRIMITIVE_QUADS);
+    host_.SetTexCoord0(0.0f, 0.0f);
+    host_.SetVertex(left, top, kZ);
+
+    host_.SetTexCoord0(kTextureSize, 0.0f);
+    host_.SetVertex(right, top, kZ);
+
+    host_.SetTexCoord0(kTextureSize, static_cast<float>(quad_height));
+    host_.SetVertex(right, bottom, kZ);
+
+    host_.SetTexCoord0(0.0f, static_cast<float>(quad_height));
+    host_.SetVertex(left, bottom, kZ);
+    host_.End();
+
+    host_.SetTextureStageEnabled(0, false);
+    host_.SetShaderStageProgram(TestHost::STAGE_NONE);
+    host_.SetBlend(true);
+  };
+
+  static constexpr uint32_t kClearColors[] = {
+      0x00DACABA, 0x011B2B3B, 0x7F3C2C1C, 0x804D5D6D, 0xFE0ECE3E, 0xFF8F9FAF,
+  };
+
+  host_.PrepareDraw(0xFF220022);
+
+  static constexpr float kLeftStart = 48.f;
+  static constexpr float kQuadSpacing = 16.f;
+  auto left = kLeftStart;
+  auto top = 128.f;
+
+  pb_print("%s\n", test_name.c_str());
+
+  for (auto clear_color : kClearColors) {
+    pb_print("0x%08X, ", clear_color);
+    clear_texture(clear_color);
+    draw_quad(left, top);
+
+    left += kTextureSize + kQuadSpacing;
+    if (left + kTextureSize >= host_.GetFramebufferWidthF()) {
+      pb_print("\n");
+      left = kLeftStart;
+      top += kTextureSize + kQuadSpacing;
+    }
+  }
+
+  pb_draw_text_screen();
+  host_.FinishDraw(allow_saving_, output_dir_, suite_name_, test_name);
 }
