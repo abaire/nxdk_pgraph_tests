4dc2b8f16749d0d9b9bd9e8c90b649944f96a662
Erik Abair <abaire@users.noreply.github.com>
2025-09-20 22:48:34 -0700

    Anisotropy tests (#252)
    * Removes unnecessary blank space when updating test sources.
    
    * Adds tests for anistropic texture filtering.

--- src/main.cpp
+++ src/main.cpp
@@ -92,6 +92,7 @@
 #include "tests/swath_width_tests.h"
 #include "tests/texgen_matrix_tests.h"
 #include "tests/texgen_tests.h"
+#include "tests/texture_anisotropy_tests.h"
 #include "tests/texture_border_color_tests.h"
 #include "tests/texture_border_tests.h"
 #include "tests/texture_cpu_update_tests.h"
@@ -456,6 +457,7 @@ static void RegisterSuites(TestHost& host, RuntimeConfig& runtime_config,
   REG_TEST(SwathWidthTests)
   REG_TEST(TexgenMatrixTests)
   REG_TEST(TexgenTests)
+  REG_TEST(TextureAnisotropyTests)
   REG_TEST(TextureBorderColorTests)
   REG_TEST(TextureBorderTests)
   REG_TEST(TextureCPUUpdateTests)
