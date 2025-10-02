0a13624902e4ba76afcc97b584857c3f18977c1d
abaire via action <abaire+github-actions@users.noreply.github.com>
2025-10-02 01:03:15 +0000

    Update Doxygen documentation

---
+++ docs/search/functions_c.js
@@ -0,0 +1,12 @@
+var searchData=
+[
+  ['tagnv2atrace_0',['TagNV2ATrace',['../d2/d3e/classTestSuite.html#a92d01e0978cf79ad0883532c73434515',1,'TestSuite']]],
+  ['teardowntest_1',['teardowntest',['../d1/d50/classColorKeyTests.html#ac1e30b83b5757a4b3c794787048eaafd',1,'ColorKeyTests::TearDownTest()'],['../d0/d9d/classLineWidthTests.html#ad667b4d45d7692d2eea33b47ac0aca75',1,'LineWidthTests::TearDownTest()'],['../d9/d5b/classStippleTests.html#a3edc61ab7aa756a927358c33311ee806',1,'StippleTests::TearDownTest()'],['../d2/d3e/classTestSuite.html#a4a67d49ad17fafd018be9871cc0910c3',1,'TestSuite::TearDownTest()'],['../dd/d22/classZMinMaxControlTests.html#ab80162ee4914ab1e1724de00e10489a6',1,'ZMinMaxControlTests::TearDownTest()']]],
+  ['texture2dascubemaptests_2',['Texture2DAsCubemapTests',['../d8/de2/classTexture2DAsCubemapTests.html#ab8f576c391c4c71f53eda0c93c677dd9',1,'Texture2DAsCubemapTests']]],
+  ['textureanisotropytests_3',['TextureAnisotropyTests',['../d5/dbc/classTextureAnisotropyTests.html#afaa759168ebac1d3990465b08687a093',1,'TextureAnisotropyTests']]],
+  ['texturebordercolortests_4',['TextureBorderColorTests',['../da/d85/classTextureBorderColorTests.html#a31ae9561d69ad434205a7e1520d4c4ae',1,'TextureBorderColorTests']]],
+  ['texturecubemaptests_5',['TextureCubemapTests',['../d1/df8/classTextureCubemapTests.html#adf56335783dbf231701c35e58d12a2a5',1,'TextureCubemapTests']]],
+  ['textureperspectiveenabletests_6',['TexturePerspectiveEnableTests',['../d1/d5c/classTexturePerspectiveEnableTests.html#a9d3257d7a833540b26fa12d57feeff34',1,'TexturePerspectiveEnableTests']]],
+  ['threedprimitivetests_7',['ThreeDPrimitiveTests',['../db/d65/classThreeDPrimitiveTests.html#a62a60092f8d90bed99bb1dfacbeec128',1,'ThreeDPrimitiveTests']]],
+  ['twodlinetests_8',['TwoDLineTests',['../db/df8/classTwoDLineTests.html#ad52dbe62d54b54caa594095f67dd5779',1,'TwoDLineTests']]]
+];
