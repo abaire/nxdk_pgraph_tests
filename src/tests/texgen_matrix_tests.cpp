deed9a54f19a7843e674708ed09cf04df05ae45f
Erik Abair <abaire@users.noreply.github.com>
2025-10-14 14:52:46 -0700

    Reenables texgen tests that may not be supported on xemu. (#278)

--- src/tests/texgen_matrix_tests.cpp
+++ src/tests/texgen_matrix_tests.cpp
@@ -23,12 +23,8 @@ static constexpr int kTextureHeight = 128;
 static std::string TestNameForTexGenMode(TextureStage::TexGen mode);
 
 static TextureStage::TexGen kTestModes[] = {
-    TextureStage::TG_DISABLE,
-    TextureStage::TG_EYE_LINEAR,
-    // TextureStage::TG_OBJECT_LINEAR, // xemu "untested" assert (generate_fixed_function) (works on HW)
-    // TextureStage::TG_SPHERE_MAP,  // xemu "channel < 2" assert (kelvin_map_texgen) (works on HW)
-    TextureStage::TG_NORMAL_MAP,
-    TextureStage::TG_REFLECTION_MAP,
+    TextureStage::TG_DISABLE,    TextureStage::TG_EYE_LINEAR, TextureStage::TG_OBJECT_LINEAR,
+    TextureStage::TG_SPHERE_MAP, TextureStage::TG_NORMAL_MAP, TextureStage::TG_REFLECTION_MAP,
 };
 
 TexgenMatrixTests::TexgenMatrixTests(TestHost &host, std::string output_dir, const Config &config)
